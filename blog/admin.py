2d11d052f876007d3a0ac66738a5f3fc18ff2193
Viktor Kernytskyi <vihtor@ukr.net>
2026-05-02 22:41:03 +0300

    Configure Django admin panel: register a post model and enable post management via the admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Post
+
 # Register your models here.
+
+admin.site.register(Post)
+#Modern version
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_date', 'published_date')
+    list_filter = ('created_date', 'published_date')
+    search_fields = ('title', 'text')

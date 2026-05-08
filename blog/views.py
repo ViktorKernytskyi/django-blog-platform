e993a1420d1ca02c02b13f194a69b587cff5f6a1
Viktor Kernytskyi <vihtor@ukr.net>
2026-05-08 13:57:44 +0300

    Filter published posts and render them in blog template

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 from django.http import HttpResponse
@@ -9,5 +11,7 @@ def post_list(request):
 
 
 def post_list_blog(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 
-    return render(request, 'blog/post_list_blog.html', {})
+    return render(request, 'blog/post_list_blog.html', { 'posts': posts
+})

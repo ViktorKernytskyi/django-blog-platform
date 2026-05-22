0e056de40db02378d3802510939076448f6e857c
Viktor Kernytskyi <vihtor@ukr.net>
2026-05-22 23:09:43 +0300

    Implement post creation workflow with Django forms

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post_list_blog/', views.post_list_blog, name='post_list_blog'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from django.http import HttpResponse
+from .forms import PostForm
 
 
 def post_list(request):
@@ -29,4 +30,28 @@ def post_detail(request, pk):
         request,
         'blog/post_detail.html',
         {'post': post}
+    )
+
+
+def post_new(request):
+    # post = get_object_or_404(Post, pk=
+
+    if request.method == "POST":
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+
+            return redirect('post_detail', pk=post.pk)
+
+    else:
+        form = PostForm()
+
+    return render(
+        request,
+        'blog/post_edit.html',
+        {'form': form}
     )
